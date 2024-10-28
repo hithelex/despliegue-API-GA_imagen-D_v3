9b1d31c94086df19290d07463bbc1fa6e05da44c
Yorlin Quispe <edinhungoygnacio@gmail.com>
2024-10-28 11:45:04 -0500

    actualizacion de un nuevo ent-point en API

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-//test
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola mundo');
+});
+
+// Nuevo endpoint para saludar a un nombre específico
+app.get('/saludo/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send(`Hola, ${nombre}`);
 });
 
 app.listen(PORT, () => {
